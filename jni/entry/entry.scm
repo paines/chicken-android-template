bb89402280b28bc7ad3e44f8a662b37bf2ffb7e5
Kristian Lein-Mathisen <kristianlein@gmail.com>
2015-12-28 23:42:37 +0100

    cleanup: embed find-extension
    it doesn't belong with the find-package.scm that runs on the host. this
    should be clearer.

--- jni/entry/entry.scm
+++ jni/entry/entry.scm
@@ -7,13 +7,38 @@
 (foreign-declare "#include <SDL.h>")
 (foreign-declare "#include \"stdio2log.c\"")
 
+
+;; ==================== bootstrapping ====================
+;; hack of the year!
+;;
+;; you can have shared libraries in your android projects (under
+;; ./libs). but only the ones that start with "lib" get included
+;; during packaging. that's great.
+;;
+;; so we have a "make libs" target which adds the "lib" prefix to all
+;; shared libraries. this hack then changes the chicken runtime to
+;; look for files with the "lib" prefix if the one without it doesn't
+;; work.
+;;
+;; include this file in your main-scheme file before any
+;; (require-library) or (use) forms!
+;;
+;; you can (use tcp) and other units, but without this patch, (use
+;; eggs) won't find the extensions.
+
+(define ##sys#find-extension
+  (let ((old-##sys#find-extension ##sys#find-extension))
+    (lambda (p inc?)
+      (or (old-##sys#find-extension p inc?)
+          (old-##sys#find-extension (string-append "lib" p) inc?)))))
+
+
 ;; start a thread which will read our stdout/stderr and forward to
 ;; logcat. this is pretty much a must-have since GLSL compile errors
 ;; etc end up on stderr usually.
 ((foreign-lambda void start_logger))
 
-;; must come before any (use ...) declarations!
-(include "./jni/chicken/find-extension.scm")
+;; ==================== your environment sould be ready here  ====================
 
 ;; replace with your favorite program here
 
